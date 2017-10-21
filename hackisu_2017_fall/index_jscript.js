96a7c56675b2d4b705f08054342ecb442481628b
Ben <bescobar@iastate.edu>
2017-10-20 22:43:59 -0500

    bad commit subfolder lol, html css jscript working on fake loading bar

---
+++ hackisu_2017_fall/index_jscript.js
@@ -0,0 +1,16 @@
+function open_menu() {
+    document.getElementById("m_side_menu").style.width = "180px";
+}
+
+function close_menu() {
+    document.getElementById("m_side_menu").style.width = "0px";
+}
+
+/*fake loading*/
+function loading_page(){
+    setTimeout(load_page,1500);
+}
+
+function load_page(){
+
+}
